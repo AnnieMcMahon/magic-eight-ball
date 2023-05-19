73be6882044e2d98fd37974a874fedc85aa0c2a0
anniemcmahon <anniemcmahon20@gmail.com>
2023-05-19 19:00:35 -0400

    added more answers

--- script.js
+++ script.js
@@ -1,37 +1,85 @@
 function magicEight() {
-  let randomNumber = Math.floor(Math.random() * 8);
+  let randomNumber = Math.floor(Math.random() * 20);
   let eightBall = '';
   switch (randomNumber) {
     case 0: {
-      eightBall = 'It is certain.';
+      eightBall = 'It is certain';
       break;
     }
     case 1: {
-      eightBall = 'It is decidedly so.';
+      eightBall = 'It is decidedly so';
       break;
     }
     case 2: {
-      eightBall = 'Reply hazy try again';
+      eightBall = 'Without a doubt';
       break;
     }
     case 3: {
-      eightBall = 'Cannot predict now';
+      eightBall = 'Yes definitely';
       break;
     }
     case 4: {
-      eightBall = 'Do not count on it';
+      eightBall = 'You may rely on it';
       break;
     }
     case 5: {
-      eightBall = 'My sources say no';
+      eightBall = 'As I see it, yes';
       break;
     }
     case 6: {
-      eightBall = 'Outlook not so good';
+      eightBall = 'Most likely';
       break;
     }
     case 7: {
+      eightBall = 'Outlook good';
+      break;
+    }
+    case 8: {
+      eightBall = 'Yes';
+      break;
+    }
+    case 9: {
       eightBall = 'Signs point to yes';
+      break;
+    }
+    case 10: {
+      eightBall = 'Reply hazy, try again';
+      break;
+    }
+    case 11: {
+      eightBall = 'Ask again later';
+      break;
+    }
+    case 12: {
+      eightBall = 'Better not tell you now';
+      break;
+    }
+    case 13: {
+      eightBall = 'Cannot predict now';
+      break;
+    }
+    case 14: {
+      eightBall = 'Concentrate and ask again';
+      break;
+    }
+    case 15: {
+      eightBall = "Don't count on it";
+      break;
+    }
+    case 16: {
+      eightBall = 'My reply is no';
+      break;
+    }
+    case 17: {
+      eightBall = 'My sources say no';
+      break;
+    }
+    case 18: {
+      eightBall = 'Outlook not so good';
+      break;
+    }
+    case 19: {
+      eightBall = 'Very doubtful';
       break;
     }
   }
